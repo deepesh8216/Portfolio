017cacc91d68d270c8066a2151fe561443fa09de
deepesh8216 <95678242+deepesh8216@users.noreply.github.com>
2025-07-15 00:44:04 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -95,7 +95,7 @@ export const HeroSection = () => {
               }}
               className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text font-medium"
             >
-              Download Resume
+              Resume
             </motion.span>
           </a>
         </div>
