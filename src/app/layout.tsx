6419c40f02b9c6bb358463d004c13c586858f0e5
deepesh8216 <95678242+deepesh8216@users.noreply.github.com>
2025-07-14 23:38:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Deepesh Mccaven",
+  description: "Created by Deepesh",
 };
 
 export default function RootLayout({
