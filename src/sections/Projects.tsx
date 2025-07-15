f3505ae0d3f25625e099d114fb43d4c111dfdb18
deepesh8216 <95678242+deepesh8216@users.noreply.github.com>
2025-07-15 13:08:30 +0530

    Update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -10,6 +10,7 @@ import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
+import { CoolMode } from "@/components/cool-mode";
 
 const portfolioProjects = [
   {
@@ -120,12 +121,14 @@ export const ProjectsSection = () => {
                     ))}
                   </ul>
                   <a href={project.link}>
+                     <CoolMode>
                     <button className="bg-white text-gray-950 h-12 w-full
                 md:w-auto px-6 rounded-xl font-semibold inline-flex items-center
                 justify-center gap-2 mt-8">
                       <span>Visit Live Site</span>
                       <ArrowUpRightIcon className="size-4" />
                     </button>
+                    </CoolMode>
                   </a>
                 </div>
                 <div className="relative">
